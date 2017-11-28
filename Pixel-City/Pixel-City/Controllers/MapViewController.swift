950306e37cd1771030a797aaf5ffb14309582f75
Ahmed Elbasha <ahmed.elbasha@outlook.com>
2017-11-28 17:42:01 +0200

    Gave the user the ability to give authority to access to user location and made the app center the mapp on user location

--- Pixel-City/Pixel-City/Controllers/MapViewController.swift
+++ Pixel-City/Pixel-City/Controllers/MapViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 import Alamofire
 import AlamofireImage
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +32,36 @@ class MapViewController: UIViewController {
     }
 
     @IBAction func centerMapButtonWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapsOnUserLocation()
+        }
     }
     
 }
 
 extension MapViewController: MKMapViewDelegate {
+    func centerMapsOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapsOnUserLocation()
+    }
 
 }
+
+
+
+
